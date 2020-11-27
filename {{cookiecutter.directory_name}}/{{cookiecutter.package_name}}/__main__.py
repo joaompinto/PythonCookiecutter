5b93a272f1258e7798b9303503413762921e7230
João Pinto <lamego.pinto@gmail.com>
2020-11-27 21:50:01 +0100

    set python.formatting.provider to black and remove broken parser sampler

--- {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/__main__.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.package_name}}/__main__.py
@@ -1,12 +1,5 @@
-from .project import Project
-from .argparser import arg_parser
-
-
 def main():
-    options, args = arg_parser()
-    project_dir = args[0]
-    proj = Project(options.name, project_dir)
-    proj.create(options.force)
+    print("Hello")
 
 
 if __name__ == "__main__":
